b6023f63fb0e3d879ea210851f7bea6d170b7b8c
Kazuro Fukuhara <kazuro.fukuhara@gmail.com>
2018-11-18 00:23:35 +0900

    add code for thread pool for coroutine.  copied from https://github.com/Kotlin/kotlin-coroutines-examples.

---
+++ src/main/kotlin/future/await.kt
@@ -0,0 +1,17 @@
+package future
+
+import java.util.concurrent.CompletableFuture
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+suspend fun <T> CompletableFuture<T>.await(): T =
+    suspendCoroutine<T> { cont: Continuation<T> ->
+        whenComplete { result, exception ->
+            if (exception == null) // the future has been completed normally
+                cont.resume(result)
+            else // the future has completed with an exception
+                cont.resumeWithException(exception)
+        }
+    }
