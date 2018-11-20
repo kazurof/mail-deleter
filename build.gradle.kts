3450752671c8f58776f66c5402a91b3842eb796c
Kazuro Fukuhara <kazuro.fukuhara@gmail.com>
2018-11-20 21:12:31 +0900

    change to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,19 @@
-apply plugin: 'java'
-apply plugin: 'application'
-apply plugin: 'com.github.ben-manes.versions'
-apply plugin: 'kotlin'
+plugins {
+  application
+  kotlin("jvm") version "1.3.10"
+  id("com.github.ben-manes.versions") version "0.20.0"
+}
 
-version = '0.0.2'
+application {
+  mainClassName = "maildeleter.Main"
+}
 
-
-
-def defaultEncoding = 'UTF-8'
-
-mainClassName = "maildeleter.Main"
-
+val version = "0.0.3"
 
 buildscript {
-  ext.kotlin_version = '1.3.10'
-
-  repositories {
-    jcenter()
-  }
   dependencies {
-    classpath 'com.github.ben-manes:gradle-versions-plugin:0.20.0'
-    classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    classpath("com.github.ben-manes:gradle-versions-plugin:0.20.0")
   }
-}
-
-compileJava {
-  options.compilerArgs = ["-proc:none"]
-}
-
-
-test {
-  testLogging.showStandardStreams = true
 }
 
 repositories {
@@ -38,23 +21,10 @@ repositories {
 }
 
 dependencies {
-  implementation "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1"
-  implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version"
-  implementation 'com.sun.mail:javax.mail:1.6.2'
-  implementation 'org.apache.logging.log4j:log4j-api:2.11.1'
-  implementation 'org.apache.logging.log4j:log4j-core:2.11.1'
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.10")
+  implementation("com.sun.mail:javax.mail:1.6.2")
+  implementation("org.apache.logging.log4j:log4j-api:2.11.1")
+  implementation("org.apache.logging.log4j:log4j-core:2.11.1")
 }
 
-
-
-
-compileKotlin {
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
-}
-compileTestKotlin {
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
-}
